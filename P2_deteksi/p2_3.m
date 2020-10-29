19e029f3976a3030f82f9816d6ebf3fcbaa29639
Bhadrika Evandito Atmomintarso <42692320+vitoatmo@users.noreply.github.com>
2020-10-29 13:11:15 +0700

    Update p2_3.m

--- P2_deteksi/p2_3.m
+++ P2_deteksi/p2_3.m
@@ -2,13 +2,16 @@
 clear all;
 close all;
 
+global Fd Fs;
+Fd = 1000; Fs = 100000; T = 1/Fd;
+b = round(rand(1,10000));
+%% P2.3.1.
 
-% 2.3.1.
-y = transmit(b, 'bipolar_nrz', 0, 550);
-detect(y, <threshold>, <sampling_time>, b, <delay>)
+y = transmit(b, 'bipolar_nrz', 0, 550); %bw=550 Hz
+ber = detect(y, 0, T, b, 1)
 
-% 2.3.2.
-y = transmit(b,'raised cosine',0,550,<roll_off>);
-ber = detect(y,<threshold>,<sampling_time>,b,20)
+%% P2.3.2.
+% y = transmit(b,'raised cosine',0,550,<roll_off>);
+% ber = detect(y,<threshold>,<sampling_time>,b,20)
 
-% 2.3.3.
+%% P2.3.3.
