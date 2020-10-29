9b248106391a93374cc2f4dcaf9e81b238be80d5
Bhadrika Evandito Atmomintarso <42692320+vitoatmo@users.noreply.github.com>
2020-10-29 20:07:39 +0700

    Update p2_3.m

--- P2_deteksi/p2_3.m
+++ P2_deteksi/p2_3.m
@@ -7,16 +7,23 @@ Fd = 1000; Fs = 100000; T = 1/Fd;
 b = round(rand(1,10000));
 %% P2.3.1.
 y = transmit(b, 'bipolar_nrz', 0, 550); %bw=550 Hz
-ber = detect(y, 0, T, b)
-
-%% P2.3.2.
+ber231a = detect(y, 0, T, b)
+y = transmit(b, 'bipolar_nrz', 0, 350); %bw=350 Hz
+ber231b = detect(y, 0, T, b)
+%% P2.3.2. dan P2.3.3.
 % y = transmit(b,'raised cosine',0,550,0.2);
 % ber = detect(y,<threshold>,<sampling_time>,b,20)
 
+% browsing
 npsd = 0 %noise;
-rolloff = 0.2;
-delay = 21;
-bw = 550;
+rolloff = 0.5;
+%^rolloff=0.2 untuk P2.3.2.
+%^rolloff=0 0.2 0.5 1.0 untuk P2.3.3.
+delay = 21; 
+%^delay=20 dan 21 untuk P2.3.2.
+%^delay=21 untuk P2.3.3.
+bw = 400; %bw = 550 450 400 350 
+%^^^ganti manual untuk P2.3.2.
 samptime = 0.001;
 th = 0;
 % 
@@ -32,12 +39,12 @@ sbl = real(ifft(Sfbl));
 chnlout = sbl + sqrt(npsd)*randn(1,ls);
 y = chnlout;
 
-plot(y)
-
+% plot(y)
+% 
 MM=length(b)*(Fs/Fd);
 sT=round(Fs*samptime);
 ysamp=y(sT:Fs/Fd:sT+Fs/Fd*(length(b)-1));
-yshifted = delayseq(ysamp,21,100);
+% yshifted = delayseq(ysamp,21,100);
 %y=y(1:MM);
 %ymat=reshape(y,(length(y))/length(b),length(b));
 %yy=ymat(sT,:);
